ae46c2dd674560e03c3ea1b3650815551be02b1f
Christian Gonzalez <cgonzalez233@yahoo.com>
2021-02-23 21:10:34 -0500

    added get route

--- client/src/components/partials/Navbar.js
+++ client/src/components/partials/Navbar.js
@@ -1,48 +1,63 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Store } from "../../store";
+import { logoutUser } from "../../store/actions/authActions";
 
 const Navbar = (props) => {
-    return (
-        <div className="navbar-fixed">
-            <nav>
-                <div class="nav-wrapper blue-grey darken-1">
-                    <a href="/" class="brand-logo">
-                        DnD Project
-                    </a>
-                    <a
-                        href="#"
-                        data-target="mobile-demo"
-                        class="sidenav-trigger"
-                    >
-                        <i class="material-icons">menu</i>
-                    </a>
-                    <ul class="right hide-on-med-and-down">
-                        <li>
-                            <Link to="/">Profile</Link>
-                        </li>
-                        <li>
-                            <Link to="/randomizer">Rondomize</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">Register</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  const { state, dispatch } = useContext(Store);
+  const user = state.auth.user || false;
+  const logout = (e) => {
+    e.preventDefault();
+    logoutUser(props.history)(dispatch);
+  };
 
-            <ul class="sidenav" id="mobile-demo">
+  return (
+    <div className="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper blue-grey darken-1">
+          <a href="/" class="brand-logo">
+            DnD Project
+          </a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+          <ul class="right hide-on-med-and-down">
+            {user ? (
+              <>
                 <li>
-                    <Link to="/">Profile</Link>
+                  <Link to="/">Profile</Link>
                 </li>
                 <li>
-                    <Link to="/randomizer">Randomize</Link>
+                  <Link to="/randomizer">Rondomize</Link>
                 </li>
-            </ul>
+                <li>
+                  <a onClick={logout}>Logout</a>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-    );
+      </nav>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li>
+          <Link to="/">Profile</Link>
+        </li>
+        <li>
+          <Link to="/randomizer">Randomize</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
