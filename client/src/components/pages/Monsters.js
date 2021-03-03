e081ec8755c3ca8e1df8fda15482d5055e7034c9
Jack Parisi <jackparisi202@gmail.com>
2021-03-02 20:20:08 -0500

    adding spell options

--- client/src/components/pages/Monsters.js
+++ client/src/components/pages/Monsters.js
@@ -6,6 +6,7 @@ import { List, ListItem } from "../../utils/List/List";
 function MonsterSearch() {
     const [monsters, setMonsters] = useState([]);
     const [monsterInfo, setMonsterInfo] = useState(false);
+
     useEffect(() => {
         API.getMonsters().then((res) => {
             setMonsters(res.data.results);
@@ -17,6 +18,7 @@ function MonsterSearch() {
             setMonsterInfo(res.data);
         });
     };
+
     return (
         <div>
             <Row>
@@ -35,47 +37,127 @@ function MonsterSearch() {
                         ))}
                     </List>
                 </Col>
-                <Col size="md-9">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">{monsterInfo.name}</span>
-                            <p>
-                                <ul>
-                                    <li>Size: {monsterInfo.size}</li>
-                                    <li>Type: {monsterInfo.type}</li>
-                                    <li>
-                                        Hit Points: {monsterInfo.hit_points}
-                                    </li>
-                                    <li>
-                                        Challenge Rating:{" "}
-                                        {monsterInfo.challenge_rating}
-                                    </li>
-                                    <li>XP: {monsterInfo.xp} Points</li>
-                                    <li>Alignment: {monsterInfo.alignment}</li>
-                                    <li>Language: {monsterInfo.languages}</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-                <Col>
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">Monster Stats</span>
-                            <p>
-                                <ul>
-                                    <li>Strength: {monsterInfo.strength}</li>
-                                    <li>Dexterity: {monsterInfo.dexterity}</li>
-                                    <li>
-                                        Intelligence: {monsterInfo.intelligence}
-                                    </li>
-                                    <li>Wisdom: {monsterInfo.wisdom}</li>
-                                    <li>Charisma: {monsterInfo.charisma}</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                </Col>
+                {monsterInfo.name ? (
+                    <>
+                        <Col size="md-9">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">
+                                        {monsterInfo.name}
+                                    </span>
+                                    <p>
+                                        <ul>
+                                            <li>Size: {monsterInfo.size}</li>
+                                            <li>Type: {monsterInfo.type}</li>
+                                            <li>
+                                                Hit Points:{" "}
+                                                {monsterInfo.hit_points}
+                                            </li>
+                                            <li>
+                                                Challenge Rating:{" "}
+                                                {monsterInfo.challenge_rating}
+                                            </li>
+                                            <li>
+                                                XP: {monsterInfo.xp} Points
+                                                Given
+                                            </li>
+                                            <li>
+                                                Alignment:{" "}
+                                                {monsterInfo.alignment}
+                                            </li>
+                                            <li>
+                                                Language:{" "}
+                                                {monsterInfo.languages}
+                                            </li>
+                                            <li>
+                                                Armor Class:{" "}
+                                                {monsterInfo.armor_class}
+                                            </li>
+                                        </ul>
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">
+                                        Monster Stats
+                                    </span>
+                                    <p>
+                                        <ul>
+                                            <li>
+                                                Strength: {monsterInfo.strength}
+                                            </li>
+                                            <li>
+                                                Dexterity:{" "}
+                                                {monsterInfo.dexterity}
+                                            </li>
+                                            <li>
+                                                Intelligence:{" "}
+                                                {monsterInfo.intelligence}
+                                            </li>
+                                            <li>
+                                                Wisdom: {monsterInfo.wisdom}
+                                            </li>
+                                            <li>
+                                                Charisma: {monsterInfo.charisma}
+                                            </li>
+                                        </ul>
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">
+                                        Special Abilities
+                                    </span>
+                                    <p>
+                                        <ul>
+                                            {monsterInfo.special_abilities.map(
+                                                ({ name, desc }) => {
+                                                    return (
+                                                        <li>
+                                                            {name}: {desc}
+                                                        </li>
+                                                    );
+                                                }
+                                            )}
+                                        </ul>
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                        {/* <Col>
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">
+                                        Proficiencies
+                                    </span>
+                                    <p>
+                                        <ul>
+                                            {monsterInfo.proficiencies.map(
+                                                ({ value, proficiency }) => {
+                                                    return (
+                                                        <li>
+                                                            Value: {value}
+                                                            Proficiency:{" "}
+                                                            {proficiency}
+                                                        </li>
+                                                    );
+                                                }
+                                            )}
+                                        </ul>
+                                    </p>
+                                </div>
+                            </div>
+                        </Col> */}
+                    </>
+                ) : (
+                    ""
+                )}
             </Row>
         </div>
     );
