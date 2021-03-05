51e26b5ad9cdcdf779d5f7a3dd79f19405caba3e
Jack Parisi <jackparisi202@gmail.com>
2021-03-05 17:45:58 -0500

    fixing styling

--- client/src/components/partials/Navbar.js
+++ client/src/components/partials/Navbar.js
@@ -5,67 +5,83 @@ import { logoutUser } from "../../store/actions/authActions";
 import "./style.css";
 
 const Navbar = (props) => {
-  const { state, dispatch } = useContext(Store);
-  const user = state.auth.user || false;
-  const history = useHistory();
-  const logout = (e) => {
-    e.preventDefault();
-    logoutUser(history)(dispatch);
-  };
+    const { state, dispatch } = useContext(Store);
+    const user = state.auth.user || false;
+    const history = useHistory();
+    const logout = (e) => {
+        e.preventDefault();
+        logoutUser(history)(dispatch);
+    };
 
-  return (
-    <div className="navbar-fixed">
-      <nav>
-        <div className="glass nav-wrapper">
-          <a href="/" className="brand-logo logo">
-            <img src="./assets/rpgenerator.png" className="logo"></img>
-          </a>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </a>
-          <ul className="right hide-on-med-and-down">
-            {user ? (
-              <>
+    return (
+        <div className="navbar-fixed">
+            <nav>
+                <div className="glass nav-wrapper">
+                    <a href="/" className="brand-logo logo">
+                        <img
+                            src="./assets/rpgenerator.png"
+                            className="logo"
+                        ></img>
+                    </a>
+                    <a
+                        href="#"
+                        data-target="mobile-demo"
+                        className="sidenav-trigger"
+                    >
+                        <i className="material-icons">menu</i>
+                    </a>
+                    <ul className="right hide-on-med-and-down">
+                        {user ? (
+                            <>
+                                <li>
+                                    <Link to="/">Profile</Link>
+                                </li>
+                                <li>
+                                    <Link to="/randomizer">Randomize</Link>
+                                </li>
+                                <li>
+                                    <Link to="/spellSearch">Spell Book</Link>
+                                </li>
+                                <li>
+                                    <Link to="/monsterSearch">Bestiary</Link>
+                                </li>
+                                <li>
+                                    <a onClick={logout}>Logout</a>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li>
+                                    <Link to="/register">Register</Link>
+                                </li>
+                                <li>
+                                    <Link to="/login">Login</Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-demo">
                 <li>
-                  <Link to="/">Profile</Link>
+                    <Link to="/">Profile</Link>
                 </li>
                 <li>
-                  <Link to="/randomizer">Randomize</Link>
+                    <Link to="/randomizer">Randomize</Link>
                 </li>
                 <li>
-                  <Link to="/spellSearch">Spell Book</Link>
+                    <Link to="/spellSearch">Spell Book</Link>
                 </li>
                 <li>
-                  <Link to="/monsterSearch">Bestiary</Link>
+                    <Link to="/monsterSearch">Bestiary</Link>
                 </li>
                 <li>
-                  <a onClick={logout}>Logout</a>
+                    <a onClick={logout}>Logout</a>
                 </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              </>
-            )}
-          </ul>
+            </ul>
         </div>
-      </nav>
-
-      <ul className="sidenav" id="mobile-demo">
-        <li>
-          <Link to="/">Profile</Link>
-        </li>
-        <li>
-          <Link to="/randomizer">Randomize</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
